ec2933b016977862ec7b6e53a1e96db0c8afab72
Guizo-X <122848312+Guizo-X@users.noreply.github.com>
2023-10-23 15:56:15 -0300

    app.py

--- automações/app.py
+++ automações/app.py
@@ -3,16 +3,8 @@ import time
 
 
 pa.press('win')
-pa.write('chrome')
+pa.write('opera')
 pa.press('enter')
 time.sleep(3)
-pa.click(x=421, y=340)
-time.sleep(3)
-pa.write("https://www.leagueofgraphs.com/pt/")
-pa.press('enter')
-time.sleep(4)
-pa.click(x=1040, y=104)
-pa.write("Ghost rid3r")
-time.sleep(3)
-pa.press('Down')
+pa.write("https://www.leagueofgraphs.com/pt/summoner/br/Ghost%20Rid3r")
 pa.press('enter')
